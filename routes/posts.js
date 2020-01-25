681640870ae1dd69552e24ca75d0c5ef5c1614e2
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-25 18:00:39 -0500

    begin working on retrieving user posts by id.

--- routes/posts.js
+++ routes/posts.js
@@ -1,49 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const pgp = require("pg-promise")();
-const connection ={
+const connection = {
   host: "localhost",
   port: 5432,
-  database: 'facebook_db'
-}
+  database: "facebook_db"
+};
+
 const db = pgp(connection);
-
-
-router.get('/all', (req, res,) => {
-  db.any('SELECT * FROM posts')
-  .then(function(data){
-    const response ={
-      posts: data
-    }
-    res.send(response)
-  })
-  .catch(function(error){
-    res.send(error)
-  })
-
+router.get("/all", (req, res) => {
+  db.any("SELECT * FROM posts")
+    .then(function(data) {
+      const response = {
+        posts: data
+      };
+      res.send(response);
+    })
+    .catch(function(error) {
+      res.send(error);
+    });
 });
+//this is where we left off
+router.get("/:user_id", async (req, res, next) => {
+  try{ 
+     let userPosts = await db.any('SELECT * FROM posts WHERE poster_id = $1',req.params.id)
+      res.status(200).json({
+        userPosts, status: 'success', message: 'all users Posts'
+      })
 
-router.get('/:user_id', async (req, res, next) => {
-  db.any
+  }catch(err){ 
+    next(err)
+  }
   // res.send('Sending all post from this user!');
 });
 
-router.post('/register', (req, res) => {
-
-  res.send('Creating new post!');
-
-
-
-
+router.post("/register", (req, res) => {
+  res.send("Creating new post!");
 });
-
-
-
-
-
-
-
-
 
 module.exports = router;
