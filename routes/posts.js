28873b80f9f1e30ce7f19dc559536804c1b2c864
Farrah Isabelle <farrahrios@pursuit.org>
2020-01-25 17:01:14 -0500

    begin backend post file

---
+++ routes/posts.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+const pgp = require("pg-promise")();
+const connection ={
+  host: "localhost",
+  port: 5432,
+  database: 'facebook_db'
+}
+const db = pgp(connection);
+
+
+router.get('/all', (req, res,) => {
+
+  db.any('SELECT * FROM posts')
+  .then(function(data){
+    const response ={
+      posts: data
+    }
+    res.send(response)
+  })
+  .catch(function(error){
+    res.send(error)
+  })
+
+});
+
+router.get('/:user_id', (req, res) => {
+  res.send('Sending all post from this user!');
+});
+
+router.post('/register', (req, res) => {
+
+  res.send('Creating new post!');
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
